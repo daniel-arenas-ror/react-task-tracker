a2db6eb2b2d1d1569d32897831fc4970c3bf69c6
Daniel Arenas <daniel1144151@gmail.com>
2025-12-07 17:21:55 +0000

    add task types

---
+++ src/types/Task.ts
@@ -0,0 +1,12 @@
+export interface Task {
+  id: number;
+  description: string;
+  created_at: string;
+  updated_at: string;
+}
+
+// Type for the data submitted via the creation form
+export interface NewTaskData {
+  description: string;
+  // We'll let the backend set the status, but include other required fields
+}
