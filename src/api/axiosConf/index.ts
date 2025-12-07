186bf976478f85955b7e91510db39d9a83f317da
Daniel Arenas <daniel1144151@gmail.com>
2025-12-07 17:43:35 +0000

    centralizate pre-configuration axion

---
+++ src/api/axiosConf/index.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import type {
+  AxiosResponse,
+  AxiosInstance, 
+  AxiosRequestConfig
+} from "axios";
+
+const DEFAULT_TIMEOUT = 5000;
+
+export class AxiosConf {
+  private http: AxiosInstance;
+
+  constructor(configuration: AxiosRequestConfig = {}) {
+    this.http = axios.create(configuration);
+  }
+
+  public instance(): AxiosInstance {
+    this.http.interceptors.response.use(
+      (response: AxiosResponse) => response.data
+    );
+
+    return this.http;
+  }
+}
+
+export default new AxiosConf({
+  // Use VITE_BACKEND_URL from environment variables (Vite-specific)
+  baseURL: import.meta.env.VITE_BACKEND_URL, 
+  timeout: DEFAULT_TIMEOUT,
+  headers: { 
+    'content-type': 'application/json' 
+  }
+}).instance();
